7e65c7a99ce4010871cc7e3fe81d4f31825665a2
Sergey <metrolog_omsk@mail.ru>
2022-07-05 22:41:53 +0500

    Решил задачу № 56

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -1,2 +1,77 @@
 ﻿// Задача 56: Задайте прямоугольный двумерный массив. 
 // Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Random rand = new Random();
+int rows = 7;
+int coulumns = 7;
+int[,] matrix = new int[rows, coulumns];
+int[] matrixSumRows = new int[rows];
+
+Console.Clear();
+Console.WriteLine($"*************************************************");
+Console.WriteLine($"Дан массив на {rows} строк (-и) и {coulumns} столбца (-ов)");
+Console.WriteLine($"*************************************************");
+
+FillArray(matrix);
+PrintArray(matrix);
+
+Console.WriteLine($"*************************************************");
+Console.WriteLine("Посчитаем сумму элементов каждой строки");
+
+SumRows(matrix, matrixSumRows);
+Console.WriteLine($"*************************************************");
+MinRowFind(matrixSumRows);
+
+Console.WriteLine($"*************************************************");
+
+void FillArray (int [,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            currentArray[i, j] = rand.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray (int[,] currentArray)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            Console.Write(currentArray[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumRows (int[,] currentArray, int[] arraySumRows)
+{
+    for (int i = 0; i < currentArray.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < currentArray.GetLength(1); j++)
+        {
+            sum += currentArray[i,j];
+        }
+        Console.WriteLine($"Сумма {i + 1} строки равна: {sum}");
+        arraySumRows[i] = sum;
+    }
+}
+
+void MinRowFind (int[] currentArray)
+{
+    int minSum = currentArray[0];
+    int minRow = 0;
+    for (int i = 0; i < currentArray.Length; i++)
+    {
+        if (currentArray[i] < minSum) 
+        {
+            minSum = currentArray[i];
+            minRow = i;
+        }
+    }
+    Console.WriteLine($"Минимальная сумма {minSum} в строке {minRow + 1}");
+}
