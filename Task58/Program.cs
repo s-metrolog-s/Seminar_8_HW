e2eea100569ee32c1ad74633ccaa4a25d51cfe45
Sergey <metrolog_omsk@mail.ru>
2022-07-08 13:55:01 +0500

    ПРобую заполнить спиралью

--- Task58/Program.cs
+++ Task58/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 58: Заполните спирально массив 4 на 4 числами от 1 до 16.
 
-int rows = 4;
-int columns = 4;
+int rows = 5;
+int columns = 5;
 int[,] matrix = new int[rows, columns];
 
 Console.Clear();
@@ -9,11 +9,13 @@ Console.WriteLine("*************************************************");
 Console.WriteLine($"Дан массив на {rows} и {columns} стролбцов");
 Console.WriteLine("*************************************************");
 
-Console.WriteLine("Заполняем массив по змейкой^");
-FillArraySnake(matrix);
-PrintArray(matrix);
+Console.WriteLine("Заполняем массив змейкой");
+//FillArraySnake(matrix);
+//PrintArray(matrix);
 
 Console.WriteLine("*************************************************");
+
+Console.WriteLine("Заполняем массив по спирали");
 FillArraySpiral(matrix);
 PrintArray(matrix);
 
@@ -39,10 +41,99 @@ void FillArraySnake (int[,] currentArray)
     }
 }
 
+
 void FillArraySpiral (int[,] currentArray)
 {
-    
+    int count = 1;
+    int row = 0;
+    int column = 1;
+    while (count < currentArray.GetLength(0) * currentArray.GetLength(1))
+    {
+        int direction = 1;
+        int circle = 0;
+        if (direction == 1)
+        {
+            for (int j = circle; j < currentArray.GetLength(1) - row + circle; j++)
+            {
+                currentArray[circle, j] = count;
+                count++;
+            }
+            direction = 2;
+            row++;
+        }
+
+        if (direction == 2)
+        {
+            for (int i = circle + 1; i < currentArray.GetLength(0) - circle; i++)
+            {
+                currentArray[i, currentArray.GetLength(1) - column + circle] = count;
+                count++;
+            }
+            direction = 3;
+            column++;
+        }
+  
+        if (direction == 3)
+        {
+            for (int j = currentArray.GetLength(1) - row - 1; j >= circle; j--)
+            {
+                currentArray[currentArray.GetLength(0) - 1 - circle, j] = count;
+                count++;
+            }
+            direction = 4;
+            row++;
+        }
+        
+        if (direction == 4)
+        {
+            for (int i = currentArray.GetLength(0) - column; i >= 1; i--)
+            {
+                currentArray[i, 0] = count;
+                count++;
+            }
+            direction = 5;
+            column++;
+        }
+        
+        circle++;
+        
+        if (direction == 5)
+        {
+            for (int j = circle; j < currentArray.GetLength(1) - row + circle; j++)
+            {
+                currentArray[circle, j] = count;
+                count++;
+            }
+            direction = 6;
+            row++;
+        }
+
+        if (direction == 6)
+        {
+            for (int i = circle + 1; i < currentArray.GetLength(0) - circle; i++)
+            {
+                currentArray[i, currentArray.GetLength(1) - column + circle] = count;
+                count++;
+            }
+            direction = 7;
+            column++;
+        }
+        
+        if (direction == 7)
+        {
+            for (int j = currentArray.GetLength(1) - row; j >= circle; j--)
+            {
+                currentArray[currentArray.GetLength(0) - 1 - circle, j] = count;
+                count++;
+            }
+            direction = 1;
+            row++;
+        }
+
+        break;
+    }
 }
+
 
 void PrintArray (int[,] currentArray)
 {
